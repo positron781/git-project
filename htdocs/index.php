901791499fd8e3ca25d9f11eb8ec8e7f9927e796
positron <m.yanko@optimism.ru>
2016-06-02 13:28:18 +0300

    new commit

--- htdocs/index.php
+++ htdocs/index.php
@@ -4,4 +4,4 @@ echo "Hello Git!";
 echo "Hello Git2!";
 echo "Hello new branch!";
 echo "branch master";
-echo "Весь мир бардак, все бабы бляди!";
+echo "Весь мир бардак, все бабы бляди! Однозначно!";
